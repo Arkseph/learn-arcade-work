bf8ab166f2f76fe3b23c393e9cb12854af155b22
Eric <ericdhr02@gmail.com>
2024-01-27 22:13:28 -0500

    lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
 print("Hello World!")
 print("It took me a long time thinking about what to write here.")
 print("This is an example sentence for Lab 01.")
-print("Let's see if the Escape Codes works:\"Nice\nGood\tOK")
+print("Let's see if the Escape Codes works:\" Nice\nGood\tOK")
