5812804c23f12b47a5085be87d4469552210b109
Eric <ericdhr02@gmail.com>
2024-01-27 22:07:26 -0500

    m

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("It took me a long time thinking about what to write here.")
+print("My computer still doesn't know who I am when I commit.")
+print("Let's see if the Escape Codes works: \"Nice\nGood\tOK")
