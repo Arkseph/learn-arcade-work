cfb2417c9ebdb76fceac90fd5c8f47d17df4b8c0
Eric <ericdhr02@gmail.com>
2024-01-27 22:10:09 -0500

    m

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
 print("Hello World!")
 print("It took me a long time thinking about what to write here.")
-print("My computer still doesn't know who I am when I commit.")
+print("This is an example sentence for Lab 01.")
 print("Let's see if the Escape Codes works: \"Nice\nGood\tOK")
