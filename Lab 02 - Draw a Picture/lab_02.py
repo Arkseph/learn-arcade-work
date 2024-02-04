c4b211485272830800b127705275cc06f119a784
Eric <ericdhr02@gmail.com>
2024-02-03 19:49:16 -0500

    lab 1

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,13 @@
+""" This is my First Drawing in
+ Python"""
+import arcade
+
+arcade.open_window(600,800,"First Drawing")
+
+arcade.set_background_color((242, 66, 12))
+
+arcade.start_render()
+
+arcade.finish_render()
+
+arcade.run()
